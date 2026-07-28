97eb186f84566cac927b217e09aab5d74caf014e
vien105200318 <105200318@sv1.dut.udn.vn>
2026-07-28 16:13:20 +0700

    feat: Class GreenCoinModel and fields: userId, balance, totalEarned, lastDailyLogin, streakDays

---
+++ lib/models/green_coin_model.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class GreenCoinModel {
+  final String userId;
+  final int balance;
+  final int totalEarned;
+  final DateTime? lastDailyLogin;
+  final int streakDays;
+
+  GreenCoinModel({
+    required this.userId,
+    required this.balance,
+    required this.totalEarned,
+    this.lastDailyLogin,
+    required this.streakDays
+  });
+  // firestorge function
+
+  factory GreenCoinModel.fromFireStore(DocumentSnapshot doc){
+    final data = doc.data() as Map<String, dynamic>? ??{};
+    return GreenCoinModel(
+      userId: doc.id, 
+      balance: data['balance']?? 0, 
+      totalEarned: data['totalEarned']?? 0, 
+      lastDailyLogin: data['lastDailyLogin'] != null
+        ? (data['lastDailyLogin'] as Timestamp).toDate()
+        :null,              
+      streakDays: data['streakDays']?? 0,
+  );
+  }
+  // corverge to map
+
+  Map<String, dynamic> toMap() {
+    return{
+      'balance' : balance,
+      'totalEarned' : totalEarned,
+      'lastDailyLogin' : lastDailyLogin != null
+        ? Timestamp.fromDate(lastDailyLogin!)
+        : null,
+
+      'streakDays' : streakDays,
+    };
+  }
+
+  // coppy new value 
+
+
+  GreenCoinModel copyWith({
+    int? balance,
+    int? totalEarnd,
+    DateTime? lastDailyLogin,
+    int? streakDays,
+  }) {
+    return GreenCoinModel(
+            userId: userId,
+            balance: balance ?? this.balance,
+            totalEarned: totalEarned ?? this.totalEarned,
+            lastDailyLogin: lastDailyLogin ?? this.lastDailyLogin,
+            streakDays: streakDays ?? this.streakDays,
+    );
+  }
+}
+
+
+/* struct firestrorge affter create:
+{
+  "balance": 0,
+  "totalEarned": 0,
+  "lastDailyLogin": null,
+  "streakDays": 0
+}
+*/
+
+
+
